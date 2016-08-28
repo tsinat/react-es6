5bede336e47aa75b600b374e71d9dbab8ffb422c
Tsinat Zeree <cheru87@gmail.com>
2016-08-28 10:25:33 -0700

    fully functional routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React , { PropTypes } from 'react';
+import Header from './common/Header';
 
 class App extends React.Component {
     render() {
         return (
             <div className='container-fluid'>
-                <p>Header here...</p>
+                <Header />
                 {this.props.children}
             </div>
         );
@@ -12,7 +13,7 @@ class App extends React.Component {
 }
 
 App.propTypes = {
-    children: PropTypes.object.isRequired;
+    children: PropTypes.object.isRequired
 };
 
 export default App;
