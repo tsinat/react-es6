9e37613d6463321049e3db3615d588a51cd527c1
Tsinat Zeree <cheru87@gmail.com>
2016-09-19 23:52:17 -0700

    adding thrunk

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -12,5 +12,4 @@ const Header = () => {
         </nav>
     );
 };
-
 export default Header;
--- src/components/course/CourseListRow.js
+++ src/components/course/CourseListRow.js
@@ -5,6 +5,15 @@ const CourseListRow = ({course}) =>{
     return (
         <tr>
             <td><a href={course.watchHref} target="_blank">Watch</a></td>
+            <td><Link to={'/course/' + course.id}>{course.title}</Link></td>
+            <td>{course.authorId}</td>
+            <td>{course.category}</td>
+            <td>{course.length}</td>
         </tr>
-    )
-}
+    );
+};
+
+CourseListRow.propTypes = {
+    course: PropTypes.object.isRequired
+};
+export default CourseListRow;
